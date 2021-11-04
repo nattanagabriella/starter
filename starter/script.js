f8c4dba7a737de08d37db6378b1bc5fe1975fbce
Nattana Gabriella <nattana_@hotmail.com>
2021-11-03 21:22:27 -0300

    modificacao1

--- starter/script.js
+++ starter/script.js
@@ -1,3 +1,13 @@
-document.querySelector(".check").addEventListener("click", function () {
-	document.querySelector(".message").textContent = `Número correto!`;
+let level = 0;
+let started = false;
+let coresBotoes = ["red", "blue", "green", "yellow"];
+let coresAleatoriasGeradas = [];
+let coresEscolhidas = [];
+
+document.addEventListener('keydown', event => {
+    if (event && !started) {
+        document.getElementById('level-title').textContent = `Level ${level}`;
+        proximaSequencia();
+        started = true;
+    }
 });
